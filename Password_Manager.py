14fdf11616169df5df5d75edd5bad7fe2e984318
Leovdw7311 <leonardvanderwalt@gmail.com>
2021-09-15 11:02:22 +0200

    Password manager

---
+++ Password_Manager.py
@@ -0,0 +1,30 @@
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+           'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+           'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator")
+print("Simply click 'Run' to generate your unhackable password...")
+
+nr_letters = 8
+nr_symbols = 3
+nr_numbers = 4
+
+password_list = []
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+password = ""
+for char in password_list:
+    password += char
+print(f"\nYour password is: {password}")
